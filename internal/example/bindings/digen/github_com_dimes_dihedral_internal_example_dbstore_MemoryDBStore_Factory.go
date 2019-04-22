8b838b4c209456bd6007e47ac178bafedba01939
Dimes <mgale4@binghamton.edu>
2019-04-22 01:02:46 -0400

    Remove generated code

--- internal/example/bindings/digen/github_com_dimes_dihedral_internal_example_dbstore_MemoryDBStore_Factory.go
+++
@@ -1,7 +0,0 @@
-package digen
-import target_pkg "github.com/dimes/dihedral/internal/example/dbstore"
-func factory_github_com_dimes_dihedral_internal_example_dbstore_MemoryDBStore(generatedComponent *GeneratedComponent) *target_pkg.MemoryDBStore {
-	target := &target_pkg.MemoryDBStore{}
-	target.Prefix = generatedComponent.provides_github_com_dimes_dihedral_internal_example_dbstore_Prefix()
-	return target
-}
